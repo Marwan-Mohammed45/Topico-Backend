3a94830b419185886f60fa57753dd722ea69fb65
Marwan-Mohammed45 <marwan.3zazy@gmail.com>
2025-07-22 21:11:20 +0300

    images

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -5,12 +5,7 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   description: { type: String },
   category: { type: String },
-  image1: { type: String },
-  image2: { type: String },
-  image3: { type: String },
-  image4: { type: String },
-  image5: { type: String },
-  image6: { type: String },
+  images: [{ type: String }], 
   stock: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
